dbbe091841d4beaa2c519ce0bdf33f0df1c1bbb0
elico <elico@Herbert>
2024-09-18 19:55:43 -0700

    Binary Printer in Progress

--- src/_03_Printing_Binary/_01_BinaryPrinter.java
+++ src/_03_Printing_Binary/_01_BinaryPrinter.java
@@ -8,7 +8,7 @@ public class _01_BinaryPrinter {
      */
 
 
-    public void printByteBinary(byte b) {
+    public static void printByteBinary(byte b) {
         // We first want to print the bit in the one's place
 
         // Shift b seven bits to the right
@@ -19,9 +19,18 @@ public class _01_BinaryPrinter {
         // Print the result using System.out.print (NOT System.out.println)
 
         //Use this method to print the remaining 7 bits of b
+    	//String binStr = decimalToBinary(b);
+    	
+    	for (int i = 0; i < 8; i++) {
+    		//System.out.println(binStr.charAt(binStr.length() - 1));
+        	byte b2 = (byte) (b >> 7 - i);
+        	b2 = (byte) (b2 & 0b1);
+        	//binStr = decimalToBinary(b);
+        	System.out.print(b2);
+    	}
     }
 
-    public void printShortBinary(short s) {
+    public static void printShortBinary(short s) {
         // Create 2 byte variables
 
         // Use bit shifting and masking (&) to save the first
@@ -30,6 +39,16 @@ public class _01_BinaryPrinter {
 
         // Call printByteBinary twice using the two bytes
         // Make sure they are in the correct order
+    	
+    	String binStr = decimalToBinary(s);
+    	
+    	byte b1 = (byte) (0b0 + Byte.parseByte((String) binStr.subSequence(0, 3)));
+    	byte b2 = (byte) (0b0 + Byte.parseByte((String) binStr.subSequence(4, 7)));
+    	
+    	printByteBinary(b1);
+    	System.out.print(" ");
+    	printByteBinary(b2);
+    	
     }
 
     public void printIntBinary(int i) {
@@ -48,6 +67,30 @@ public class _01_BinaryPrinter {
     }
 
     public static void main(String[] args) {
-        // Test your methods here
+       // printByteBinary((byte) 100);
+       // System.out.println();
+       // printByteBinary((byte) -69);
+    	printShortBinary((short) 164);
     }
+    
+    public static String decimalToBinary(int decimalNum) {
+    	String binaryStr = "";
+    	
+    	do {
+    		
+    		int quotient = decimalNum >>> 1;
+    		
+    		if (decimalNum % 2 != 0) {
+    			binaryStr = '1' + binaryStr;
+    		} else {
+    			binaryStr = '0' + binaryStr;
+    		}
+    		
+    		decimalNum = quotient;
+    		
+    	} while(decimalNum != 0);
+    	
+    	return binaryStr;
+    }
+    
 }
